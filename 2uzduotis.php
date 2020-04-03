a1690892644b8f1246540b565c3be2d3c1eb2bca
Auguste <Auguste.alm@gmail.com>
2020-04-03 10:02:13 +0300

    Initial commit

---
+++ 2uzduotis.php
@@ -0,0 +1,117 @@
+<?php
+$mokiniai = [
+    "6a" => [
+        "1Mok" => [
+            "Kodas" => '',
+            "Vardas" => "algis",
+            "Pavarde" => "algiukas",
+            "DuomForDat" => '',
+            "Kontroliniai" => [
+                "Matematika" => 5,
+                "Informacines technologijos" => 9,
+                "Anglu kalba" => 10
+                 ]
+            ],
+         "2Mok" =>  [
+            "Kodas" => '',
+            "Vardas" => "maryte",
+            "Pavarde" => "marionaite",
+            "DuomForDat" => '',
+            "Kontroliniai" => [
+                 "Matematika" => 9,
+                 "Informacines technologijos" => 9,
+                 "Anglu kalba" => 6
+                 ]
+            ],
+         "3Mok" =>  [
+            "Kodas" => '',
+            "Vardas" => "kostas",
+            "Pavarde" => "kostauskis",
+            "DuomForDat" => '',
+            "Kontroliniai" => [
+                "Matematika" => 7,
+                "Informacines technologijos" => 8,
+                "Anglu kalba" => 6
+                 ]
+            ],
+         "4Mok" =>  [
+            "Kodas" => '',
+            "Vardas" => "giedre",
+            "Pavarde" => "giedrulaite",
+            "DuomForDat" => '',
+            "Kontroliniai" => [
+                "Matematika" => 6,
+                "Informacines technologijos" => 6,
+                "Anglu kalba" => 7
+                 ]
+            ],
+         "5Mok" =>  [
+            "Kodas" => '',
+            "Vardas" => "edas",
+            "Pavarde" => "edinskas",
+            "DuomForDat" => '',
+            "Kontroliniai" => [
+                "Matematika" => 5,
+                "Informacines technologijos" => 6,
+                "Anglu kalba" => 7
+                ]
+            ]
+        ],
+    "6b" => [
+        "1Mok" => [
+                "Kodas" => '',
+                "Vardas" => "stefa",
+                "Pavarde" => "stefulaite",
+                "DuomForDat" => '',
+                "Kontroliniai" => [
+                    "Matematika" => 10,
+                    "Informacines technologijos" => 10,
+                    "Anglu kalba" => 10
+                     ]
+                ],
+             "2Mok" =>  [
+                "Kodas" => '',
+                "Vardas" => "ula",
+                "Pavarde" => "ulinskyte",
+                "DuomForDat" => '',
+                "Kontroliniai" => [
+                    "Matematika" => 8,
+                    "Informacines technologijos" => 9,
+                    "Anglu kalba" => 7
+                     ]
+                ],
+             "3Mok" =>  [
+                "Kodas" => '',
+                "Vardas" => "kipras",
+                "Pavarde" => "kiparisas",
+                "DuomForDat" => '',
+                "Kontroliniai" => [
+                    "Matematika" => 7,
+                    "Informacines technologijos" => 6,
+                    "Anglu kalba" => 6
+                    ]
+                ],
+             "4Mok" =>  [
+                "Kodas" => '',
+                "Vardas" => "andrius",
+                "Pavarde" => "andriukaitis",
+                "DuomForDat" => '',
+                "Kontroliniai" => [
+                    "Matematika" => 9,
+                    "Informacines technologijos" => 9,
+                    "Anglu kalba" => 10
+                    ]
+                ],
+             "5Mok" =>  [
+                 "Kodas" => '',
+                 "Vardas" => "mantas",
+                "Pavarde" => "mantulionis",
+                "DuomForDat" => '',
+                "Kontroliniai" => [
+                    "Matematika" => 5,
+                    "Informacines technologijos" => 9,
+                    "Anglu kalba" => 10
+                    ]
+                ]
+             ]
+        ];
