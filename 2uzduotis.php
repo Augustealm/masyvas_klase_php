2a6f653c6e58e3d6fdb0f0867902527ac22d835b
Auguste <Auguste.alm@gmail.com>
2020-04-17 00:13:58 +0300

    .

--- 2uzduotis.php
+++ 2uzduotis.php
@@ -1,117 +1,167 @@
+<!doctype html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Mokaus PHP</title>
+</head>
+<body>
 <?php
+
+function kodas() {
+    $mokKodas = rand(100, 200);
+    return $mokKodas;
+};
+
+function pazymys() {
+    $paz = rand(1, 10);
+    return $paz;
+};
+
+function DuomForDat()
+{
+    return date("Y-m-d");
+}
+
+
 $mokiniai = [
     "6a" => [
-        "1Mok" => [
-            "Kodas" => '',
-            "Vardas" => "algis",
-            "Pavarde" => "algiukas",
-            "DuomForDat" => '',
-            "Kontroliniai" => [
-                "Matematika" => 5,
-                "Informacines technologijos" => 9,
-                "Anglu kalba" => 10
+        ["Kodas" => kodas(),
+        "Vardas" => "algis",
+        "Pavarde" => "algiukas",
+        "DuomForDat" => DuomForDat(),
+        "Kontroliniai" => [
+            "Matematika" => pazymys(),
+            "Informacines technologijos" => pazymys(),
+            "Anglu kalba" => pazymys()
+             ]
+        ],
+        ["Kodas" => kodas(),
+         "Vardas" => "maryte",
+         "Pavarde" => "marionaite",
+         "DuomForDat" => DuomForDat(),
+         "Kontroliniai" => [
+             "Matematika" => pazymys(),
+             "Informacines technologijos" => pazymys(),
+             "Anglu kalba" => pazymys()
+             ]
+        ],
+        ["Kodas" => kodas(),
+        "Vardas" => "kostas",
+        "Pavarde" => "kostauskis",
+        "DuomForDat" => DuomForDat(),
+        "Kontroliniai" => [
+            "Matematika" => pazymys(),
+            "Informacines technologijos" => pazymys(),
+            "Anglu kalba" => pazymys()
+             ]
+        ],
+        ["Kodas" => kodas(),
+        "Vardas" => "giedre",
+        "Pavarde" => "giedrulaite",
+        "DuomForDat" => DuomForDat(),
+        "Kontroliniai" => [
+                "Matematika" => pazymys(),
+                "Informacines technologijos" => pazymys(),
+                "Anglu kalba" => pazymys()
                  ]
-            ],
-         "2Mok" =>  [
-            "Kodas" => '',
-            "Vardas" => "maryte",
-            "Pavarde" => "marionaite",
-            "DuomForDat" => '',
-            "Kontroliniai" => [
-                 "Matematika" => 9,
-                 "Informacines technologijos" => 9,
-                 "Anglu kalba" => 6
-                 ]
-            ],
-         "3Mok" =>  [
-            "Kodas" => '',
-            "Vardas" => "kostas",
-            "Pavarde" => "kostauskis",
-            "DuomForDat" => '',
-            "Kontroliniai" => [
-                "Matematika" => 7,
-                "Informacines technologijos" => 8,
-                "Anglu kalba" => 6
-                 ]
-            ],
-         "4Mok" =>  [
-            "Kodas" => '',
-            "Vardas" => "giedre",
-            "Pavarde" => "giedrulaite",
-            "DuomForDat" => '',
-            "Kontroliniai" => [
-                "Matematika" => 6,
-                "Informacines technologijos" => 6,
-                "Anglu kalba" => 7
-                 ]
-            ],
-         "5Mok" =>  [
-            "Kodas" => '',
-            "Vardas" => "edas",
-            "Pavarde" => "edinskas",
-            "DuomForDat" => '',
-            "Kontroliniai" => [
-                "Matematika" => 5,
-                "Informacines technologijos" => 6,
-                "Anglu kalba" => 7
+        ],
+        ["Kodas" => kodas(),
+         "Vardas" => "edas",
+         "Pavarde" => "edinskas",
+         "DuomForDat" => DuomForDat(),
+         "Kontroliniai" => [
+                "Matematika" => pazymys(),
+                "Informacines technologijos" => pazymys(),
+                "Anglu kalba" => pazymys()
                 ]
+         ]
+    ],
+    "6b" => [
+        ["Kodas" => kodas(),
+        "Vardas" => "stefa",
+        "Pavarde" => "stefulaite",
+        "DuomForDat" => DuomForDat(),
+        "Kontroliniai" => [
+            "Matematika" => pazymys(),
+            "Informacines technologijos" => pazymys(),
+            "Anglu kalba" => pazymys()
+             ]
+        ],
+        ["Kodas" => kodas(),
+        "Vardas" => "ula",
+        "Pavarde" => "ulinskyte",
+        "DuomForDat" => DuomForDat(),
+        "Kontroliniai" => [
+            "Matematika" => pazymys(),
+            "Informacines technologijos" =>pazymys(),
+            "Anglu kalba" => pazymys()
+             ]
+        ],
+        ["Kodas" => kodas(),
+        "Vardas" => "kipras",
+        "Pavarde" => "kiparisas",
+        "DuomForDat" => DuomForDat(),
+        "Kontroliniai" => [
+            "Matematika" => pazymys(),
+            "Informacines technologijos" => pazymys(),
+            "Anglu kalba" => pazymys()
             ]
         ],
-    "6b" => [
-        "1Mok" => [
-                "Kodas" => '',
-                "Vardas" => "stefa",
-                "Pavarde" => "stefulaite",
-                "DuomForDat" => '',
-                "Kontroliniai" => [
-                    "Matematika" => 10,
-                    "Informacines technologijos" => 10,
-                    "Anglu kalba" => 10
-                     ]
-                ],
-             "2Mok" =>  [
-                "Kodas" => '',
-                "Vardas" => "ula",
-                "Pavarde" => "ulinskyte",
-                "DuomForDat" => '',
-                "Kontroliniai" => [
-                    "Matematika" => 8,
-                    "Informacines technologijos" => 9,
-                    "Anglu kalba" => 7
-                     ]
-                ],
-             "3Mok" =>  [
-                "Kodas" => '',
-                "Vardas" => "kipras",
-                "Pavarde" => "kiparisas",
-                "DuomForDat" => '',
-                "Kontroliniai" => [
-                    "Matematika" => 7,
-                    "Informacines technologijos" => 6,
-                    "Anglu kalba" => 6
-                    ]
-                ],
-             "4Mok" =>  [
-                "Kodas" => '',
-                "Vardas" => "andrius",
-                "Pavarde" => "andriukaitis",
-                "DuomForDat" => '',
-                "Kontroliniai" => [
-                    "Matematika" => 9,
-                    "Informacines technologijos" => 9,
-                    "Anglu kalba" => 10
-                    ]
-                ],
-             "5Mok" =>  [
-                 "Kodas" => '',
-                 "Vardas" => "mantas",
-                "Pavarde" => "mantulionis",
-                "DuomForDat" => '',
-                "Kontroliniai" => [
-                    "Matematika" => 5,
-                    "Informacines technologijos" => 9,
-                    "Anglu kalba" => 10
-                    ]
-                ]
-             ]
-        ];
+        ["Kodas" => kodas(),
+        "Vardas" => "andrius",
+        "Pavarde" => "andriukaitis",
+        "DuomForDat" => DuomForDat(),
+        "Kontroliniai" => [
+            "Matematika" => pazymys(),
+            "Informacines technologijos" => pazymys(),
+            "Anglu kalba" => pazymys()
+            ]
+        ],
+        ["Kodas" => kodas(),
+         "Vardas" => "mantas",
+         "Pavarde" => "mantulionis",
+         "DuomForDat" => DuomForDat(),
+         "Kontroliniai" => [
+            "Matematika" => pazymys(),
+            "Informacines technologijos" => pazymys(),
+            "Anglu kalba" => pazymys()
+            ]
+         ]
+       ]
+    ];
+
+
+?>
+<table>
+    <tr>
+        <th>Klasė</th>
+        <th>Kodas</th>
+        <th>Vardas</th>
+        <th>Pavardė</th>
+        <th>Kontrolinių <br>vidurkis</br> </th>
+        <th>Data</th>
+    </tr>
+
+<?php
+foreach ($mokiniai as $klase => $mok): ?>
+    <tr>
+        <td  colspan="1"> <?=$klase; ?> </td>
+        <td colspan="5"> </td>
+    </tr>
+    <?php
+    foreach ($mok as $duom) :?>
+    <tr>
+        <td>  </td>
+        <td> <?= $duom["Kodas"] ?></td>
+        <td> <?= mb_strtoupper($duom["Vardas"]) ?></td>
+        <td> <?= mb_strtoupper($duom["Pavarde"]) ?></td>
+        <td> <?= round(array_sum($duom["Kontroliniai"]) / count($duom["Kontroliniai"])) ?> </td>
+        <td> <?=$duom["DuomForDat"] ?></td>
+    </tr>
+<?php endforeach;
+         endforeach; ?>
+</table>
+</body>
+</html>
+
